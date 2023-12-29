d30a10ae1d6c04450135ab81f55695139fdcee29
Jacob Cottam <jacobcottam@gmail.com>
2023-12-29 16:36:07 +0000

    fix: astro config

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,14 @@
 import { defineConfig } from "astro/config";
-
 import solidJs from "@astrojs/solid-js";
 import tailwind from "@astrojs/tailwind";
 import icon from "astro-icon";
 
+import vercel from "@astrojs/vercel/serverless";
+
 // https://astro.build/config
 export default defineConfig({
+  site: "https://jacobcottam.xyz",
   integrations: [solidJs(), tailwind(), icon()],
+  output: "server",
+  adapter: vercel()
 });
