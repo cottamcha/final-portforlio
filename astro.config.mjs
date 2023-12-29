c5cbd1ca055c070c053c76eb7d5ae96cd765c722
Jacob Cottam <jacobcottam@gmail.com>
2023-12-29 19:14:44 +0000

    build fixes

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,5 +6,12 @@ import icon from "astro-icon";
 // https://astro.build/config
 export default defineConfig({
   site: "https://jacobcottam.xyz",
+  vite: {
+    build: {
+      rollupOptions: {
+        external: ["tslib"],
+      },
+    },
+  },
   integrations: [solidJs(), tailwind(), icon()],
 });
