554077e19fe04248edb897b8e91dbed14ac1a0e2
Jacob Cottam <jacobcottam@gmail.com>
2023-12-29 16:47:22 +0000

    change to static

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,12 +3,8 @@ import solidJs from "@astrojs/solid-js";
 import tailwind from "@astrojs/tailwind";
 import icon from "astro-icon";
 
-import vercel from "@astrojs/vercel/serverless";
-
 // https://astro.build/config
 export default defineConfig({
   site: "https://jacobcottam.xyz",
   integrations: [solidJs(), tailwind(), icon()],
-  output: "server",
-  adapter: vercel()
 });
