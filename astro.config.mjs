d2e498ea7d86a09fca502749463f89e42b2e1d8a
Jacob Cottam <jacobcottam@gmail.com>
2023-12-31 15:53:05 +0000

    tw to unocss

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,14 +1,13 @@
 import { defineConfig } from "astro/config";
 import solidJs from "@astrojs/solid-js";
-import tailwind from "@astrojs/tailwind";
 import icon from "astro-icon";
-
+import UnoCSS from "@unocss/astro";
 import netlify from "@astrojs/netlify";
 
 // https://astro.build/config
 export default defineConfig({
   site: "https://jacobcottam.xyz",
-  integrations: [solidJs(), tailwind(), icon()],
+  integrations: [solidJs(), icon(), UnoCSS({ injectReset: true })],
   output: "server",
   adapter: netlify(),
 });
