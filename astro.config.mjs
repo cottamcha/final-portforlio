629e982f880604da7ac45cbab1ca61df531360c5
Jacob Cottam <jacobcottam@gmail.com>
2023-12-31 15:57:36 +0000

    asd

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,11 +2,12 @@ import { defineConfig } from "astro/config";
 import solidJs from "@astrojs/solid-js";
 import icon from "astro-icon";
 import UnoCSS from "@unocss/astro";
-import netlify from "@astrojs/netlify";
+import vercel from "@astrojs/vercel/serverless";
 
 // https://astro.build/config
 export default defineConfig({
   site: "https://jacobcottam.xyz",
   integrations: [solidJs(), icon(), UnoCSS({ injectReset: true })],
-  output: "static",
+  output: "server",
+  adapter: vercel(),
 });
